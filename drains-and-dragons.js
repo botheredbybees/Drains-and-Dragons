9a076ae3d2f6541aa78a501a2e300d178aed23b8
Brian Jones <ba.jones@me.com>
2015-07-03 21:46:59 +1000

    Implemented leaflet map view entered on Hobart

--- drains-and-dragons.js
+++ drains-and-dragons.js
@@ -1,23 +1,19 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
+$(window).resize(function () {
+  var h = $(window).height(), offsetTop = 90; // Calculate the top offset
+  $mc = $('#map_canvas');
+  $mc.css('height', (h - offsetTop));
+}).resize();
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
 }
+map = L.map($('#map_canvas'), {
+  doubleClickZoom: false,
+  touchZoom: false
+}).setView(new L.LatLng(-42.8806, 147.3250), 13);
+
+L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png', {opacity: .5}).addTo(map);

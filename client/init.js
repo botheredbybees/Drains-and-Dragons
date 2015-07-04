5b5b31448e2ef2062fdb92606962c4965d7a5e4a
Robin Petterd <robin@sproutlabs.com.au>
2015-07-04 13:44:50 +1000

    starting to get leaflet to work again.

--- client/init.js
+++ client/init.js
@@ -1,5 +1,53 @@
+// on startup run resizing event
 Meteor.startup(function() {
-  // Potentially prompts the user to enable location services. We do this early
-  // on in order to have the most accurate location by the time the user shares
-  Geolocation.currentLocation();
+	Geolocation.currentLocation();
+  $(window).resize(function() {
+    $('#map').css('height', window.innerHeight - 82 - 45);
+  });
+  $(window).resize(); // trigger resize event
 });
+
+// create marker collection
+var Markers = new Meteor.Collection('markers');
+
+Meteor.subscribe('markers');
+
+Template.map.rendered = function() {
+  L.Icon.Default.imagePath = 'packages/bevanhunt_leaflet/images';
+
+  var map = L.map('map', {
+    doubleClickZoom: false
+  });
+
+ // map.locate({setView: true, maxZoom: 16});
+
+
+  L.tileLayer.provider('Thunderforest.Outdoors').addTo(map);
+
+  map.on('dblclick', function(event) {
+    Markers.insert({latlng: event.latlng});
+  });
+
+  var query = Markers.find();
+  query.observe({
+    added: function (document) {
+      var marker = L.marker(document.latlng).addTo(map)
+        .on('click', function(event) {
+          map.removeLayer(marker);
+          Markers.remove({_id: document._id});
+        });
+    },
+    removed: function (oldDocument) {
+      layers = map._layers;
+      var key, val;
+      for (key in layers) {
+        val = layers[key];
+        if (val._latlng) {
+          if (val._latlng.lat === oldDocument.latlng.lat && val._latlng.lng === oldDocument.latlng.lng) {
+            map.removeLayer(val);
+          }
+        }
+      }
+    }
+  });
+};

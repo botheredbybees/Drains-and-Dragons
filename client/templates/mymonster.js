c4b297535ce1224d355963444cdafba7279e590d
botheredbybees <botheredbybees@gmail.com>
2015-07-05 15:24:09 +1000

    finialised fighting

--- client/templates/mymonster.js
+++ client/templates/mymonster.js
@@ -168,10 +168,15 @@ Template.monster_fight.events({
     	damage = Math.floor((Math.random() * 10) + 1);    	
       Session.set("monsterhealth",Session.get("monsterhealth")-damage);
       if (Session.get("monsterhealth") <= 0) {
+      		sAlert.closeAll();
       		Router.go('monster_died');
       }
       suffer = Math.floor((Math.random() * 10) + 1);
       Session.set("playerhealth", Session.get("playerhealth")-suffer);
+      if (Session.get("playerhealth") <= 0) {
+      	sAlert.closeAll();
+      	Router.go('you_died');
+      }
     	switch(damage) {
 		    case 10:
 		    case 9:
@@ -210,10 +215,16 @@ Template.monster_fight.events({
 Router.route('monster_died');
 
 Template.monster_died.helpers({
+	ourHealth: function() {
+		return Session.get("playerhealth");
+	},
+	ourmonstersHealth: function() {
+		return Session.get("monsterhealth");
+	},
   getmymonster: function() {
-  	console.log('looking for monsters');
-  	console.log("2 "+Session.get("selectedMonster"));
-  	console.log(Monsters.findOne({name: Session.get("selectedMonster")}));
+  	// console.log('looking for monsters');
+  	// console.log("2 "+Session.get("selectedMonster"));
+  	// console.log(Monsters.findOne({name: Session.get("selectedMonster")}));
     return Monsters.findOne({name: Session.get("selectedMonster")});
   },
   fighting: function() {
@@ -222,6 +233,76 @@ Template.monster_died.helpers({
 });
 
 Template.monster_died.events({
+    'click .fight': function(){
+        //console.log("You clicked fight");
+        Session.set("monsterhealth",100);
+        console.log(Session.get("monsterhealth"));
+    },
+    'click .flee': function(){
+        Router.go('/');
+    },
+    'click .findout': function(){
+       // console.log("You clicked find out more");
+  		var monster = Monsters.findOne({name: Session.get("selectedMonster")});
+  		//console.log(monster.setting);
+  		switch(monster.setting) {
+		    case "Cavern Dwellers":
+		        Session.set("monsterfile","cavern_dwellers");
+		        break;
+		    case "Folk of the Realm":		        
+		        Session.set("monsterfile","folk_realm");
+		        break;
+		    case "Lower Depths":		        
+		        Session.set("monsterfile","lower_depths");
+		        break;
+		    case "Twisted Experiments":		        
+		        Session.set("monsterfile","twisted_experiments");
+		        break;
+		    case "Ravenous Hordes":		        
+		        Session.set("monsterfile","ravenous_hordes");
+		        break;
+		    case "Planar Powers":		        
+		        Session.set("monsterfile","planar_powers");
+		        break;
+		    case "Swamp Denizens":		        
+		        Session.set("monsterfile","denizens_swamp");
+		        break;
+		    case "Undead Legions":		        
+		        Session.set("monsterfile","legions_undead");
+		        break;
+		    case "Dark Woods":		        
+		        Session.set("monsterfile","dark_woods");
+		        break;
+		    default:		        
+		        Session.set("monsterfile","planar_powers");
+		};
+		return;
+    }
+});
+
+//////////////////////////////// your died ///////////////////////////////////////////
+
+Router.route('you_died');
+
+Template.you_died.helpers({
+	ourHealth: function() {
+		return Session.get("playerhealth");
+	},
+	ourmonstersHealth: function() {
+		return Session.get("monsterhealth");
+	},
+  getmymonster: function() {
+  	// console.log('looking for monsters');
+  	// console.log("2 "+Session.get("selectedMonster"));
+  	// console.log(Monsters.findOne({name: Session.get("selectedMonster")}));
+    return Monsters.findOne({name: Session.get("selectedMonster")});
+  },
+  fighting: function() {
+  		return Session.get("fighting");
+  }
+});
+
+Template.you_died.events({
     'click .fight': function(){
         //console.log("You clicked fight");
         Session.set("monsterhealth",100);
